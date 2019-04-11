e9fcc24450da199cc6e661fc79799637302ec518
Arthur Dadalto <arthurpd10@gmail.com>
2019-04-11 23:34:46 +0000

    Environment configuration and dinic algorithm

---
+++ flow/dinic/applications/matching.cpp
@@ -0,0 +1,27 @@
+// http://www.spoj.com/problems/MATCHING/
+
+#include "../dinic.cpp"
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    int a, b, l, r, m;
+    scanf("%d %d %d", &l, &r, &m);
+
+    dinic::init(l + r + 2, l + r, l + r + 1);
+
+    for (int i = 0; i < m; i++)
+    {
+        scanf("%d %d", &a, &b);
+        a--, b--;
+        dinic::put_edge(a, b + l, 1);
+    }
+
+    for (int i = 0; i < l; i++)
+        dinic::put_edge(l + r, i, 1);
+    for (int i = l; i < l + r; i++)
+        dinic::put_edge(i, l + r + 1, 1);
+
+    cout << dinic::max_flow() << endl;
+}
