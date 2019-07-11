1c19cfb1df97986d206fc936e0a66a0cb9dfd1e5
Arthur Pratti Dadalto <arthurpd10@gmail.com>
2019-07-11 13:53:32 -0300

    BIT

---
+++ data_structures/bit/applications/489e.cpp
@@ -0,0 +1,37 @@
+#include "../bit.cpp"
+
+int main(void)
+{
+	int n, q;
+	cin >> n >> q;
+	vector<int> a(n + 1);
+	bit<long long> bit(n);
+	for (int i = 1; i <= n; i++)
+	{
+		scanf("%d", &a[i]);
+		bit.update(i, a[i]);
+	}
+
+	int p, x;
+	for (int i = 0; i < q; i++)
+	{
+		scanf("%d %d", &p, &x);
+		bit.update(p, x - a[p]);
+		a[p] = x;
+
+		long long sum = 0;
+		while (true)
+		{
+			p = bit.lower_bound(sum);
+			if (p > n)
+				break;
+
+			if (bit.query(p) == 2 * bit.query(p - 1))
+				break;
+			
+			sum = 2 * bit.query(p);
+		}
+
+		printf("%d\n", p > n ? -1 : p);
+	}
+}
---
+++ data_structures/bit/bit.cpp
@@ -0,0 +1,42 @@
+#include "../../contest/header.hpp"
+
+/*
+	BIT: element update, range sum query and sum lower_bound in O(log(N)).
+*/
+
+template <class T>
+struct bit
+{
+	int n, LOGN;
+	vector<T> val;
+	bit(int _n) : n(_n), LOGN(log2(n + 1)), val(_n + 1, 0) {}
+
+	// val[pos] += x
+	void update(int pos, T x)
+	{
+		for (int i = pos; i <= n; i += -i & i)
+			val[i] += x;
+	}
+
+	// sum of range [1, pos]
+	T query(int pos)
+	{
+		T retv = 0;
+		for (int i = pos; i > 0; i -= -i & i)
+			retv += val[i];
+		return retv;
+	}
+
+	// min pos such that sum of [1, pos] >= sum, or n + 1 if none exists.
+	int lower_bound(T x)
+	{
+		T sum = 0;
+		int pos = 0;
+
+		for (int i = LOGN; i >= 0; i--)
+			if (pos + (1 << i) <= n && sum + val[pos + (1 << i)] < x)
+				sum += val[pos += (1 << i)];
+
+		return pos + 1; // pos will have position of largest value less than x.
+	}
+};
