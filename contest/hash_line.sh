cabcdae6194ca4951ee7d827e73b78b0f70f3816
Arthur Pratti Dadalto <arthurpd10@gmail.com>
2019-09-09 16:11:51 -0300

    Update hash script

---
+++ contest/hash_line.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for i in $(seq 1 `wc -l < $1`); do 
+	echo -en "$i\t";
+	sed -n $i'p' $1 | cpp -dD -P -fpreprocessed | tr -d '[:space:]' | md5sum | cut -c-6 | tr -d '\n'; 
+	echo -en '\t';
+	sed -n $i'p' $1;
+done
