0da4d5df2283b081bd4c2ceafc7032dfb8a7d16d
Arthur Pratti Dadalto <arthurpd10@gmail.com>
2019-09-12 11:43:14 -0300

    Hash ignores include

--- contest/hash_file.sh
+++ contest/hash_file.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-cpp -dD -P -fpreprocessed | tr -d '[:space:]'| md5sum | cut -c-6
+# Hashes a file, ignoring lines with #include, all whitespace and comments
+
+sed -e "/#include/d" | cpp -dD -P -fpreprocessed | tr -d '[:space:]' | md5sum | cut -c-6
--- contest/hash_line.sh
+++ contest/hash_line.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Hashes each line of a file, ignoring all whitespace and comments (multi-line comments will be bugged).
 
 while IFS= read -r line; do # Loops lines of stdin.
 	echo "$line" | cpp -dD -P -fpreprocessed | tr -d '[:space:]' | md5sum | cut -c-6 | tr -d '[:space:]';
